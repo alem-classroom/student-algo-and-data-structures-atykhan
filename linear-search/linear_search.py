da0bcfc8e1f7e23ff2b04b29b9cd8137bae2cd1e
atykhan <magzhan.atykhan@nu.edu.kz>
2020-10-16 10:49:00 +0600

    trial

--- linear-search/linear_search.py
+++ linear-search/linear_search.py
@@ -3,12 +3,15 @@ def linear_search(lst, to_find):
   # if found, return index of element
   # else return -1
 	a = int(len(lst))
-	for i in range (a):
-		b = -1
-		if lst[i] == to_find:
-			b = i
-			break
-	return(b)
+	if to_find in lst:
+		for i in range (a):
+			if to_find == lst[i]:
+				b = lst[i]
+				break
+		return(b)
+	return(-1)
 # alph = [1,2,3,24,5,6]
 # ind = 55
 # print(linear_search(alph,ind))
+# words = ["keyboard","клавиатура","mouse","мышка","computer","компьютер","printer","принтер","monitor","монитор"]
+# print(linear_search(words,"computer"))
