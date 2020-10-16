31ad5ccaecbbb1c6d899a5122325ba7891e6bab3
atykhan <magzhan.atykhan@nu.edu.kz>
2020-10-16 10:23:17 +0600

    trial

--- linear-search/linear_search.py
+++ linear-search/linear_search.py
@@ -2,10 +2,13 @@ def linear_search(lst, to_find):
   # search for the element to_find inside lst
   # if found, return index of element
   # else return -1
-    if to_find in lst:
-    	return(lst.index(to_find))
-    else:
-    	return(-1)
+	a = int(len(lst))
+	for i in range (a):
+		b = -1
+		if to_find == lst[i]:
+			b = i
+			return(b)
+			break
 # alph = [1,2,3,24,5,6]
-# ind = 1
+# ind = 5
 # print(linear_search(alph,ind))
