6ae2678be699bc3388bfb56ba0ccb66aeceb3c80
atykhan <magzhan.atykhan@nu.edu.kz>
2020-10-16 11:42:23 +0600

    final

--- binary-search/binary_search.py
+++ binary-search/binary_search.py
@@ -4,14 +4,16 @@ def binary_search(lst, to_find):
     # else return -1
 	a = int(len(lst))
 	b = int(a/2)
-	for i in range (b):
-		B = lst[:len(lst)//2]
-		C = lst[len(lst)//2:]
-		if to_find in B:
-			lst = B
-		elif to_find in C:
-			lst = C
-	return(lst[0])
+	if to_find in lst:
+		for i in range (b):
+			B = lst[:len(lst)//2]
+			C = lst[len(lst)//2:]
+			if to_find in B:
+				lst = B
+			elif to_find in C:
+				lst = C
+		return(lst[0])
+	return(-1)
 
 # listt = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 # asd = 9
