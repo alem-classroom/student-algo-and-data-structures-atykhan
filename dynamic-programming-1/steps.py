9e6578bab40496bac0a5f9bcb2273518457b0c30
atykhan <magzhan.atykhan@nu.edu.kz>
2020-10-16 14:31:20 +0600

    trial

--- dynamic-programming-1/steps.py
+++ dynamic-programming-1/steps.py
@@ -1,3 +1,19 @@
 def steps(num):
     # solve the problem via dynamic programming
     # return number of required steps
+    fprev = 0
+    fprev2 = 1
+    if num == 0:
+    	return(0)
+    elif num == 1:
+    	return(1)
+    else:
+    	for i in range (num):
+    		fsled = fprev2 + fprev
+    		fprev = fprev2
+    		fprev2 = fsled
+    	return(fprev)
+
+# print(steps(11))
+
+
