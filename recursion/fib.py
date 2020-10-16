1f5bc80a7475e3c2cb17559f6ce8ea8425149a75
atykhan <magzhan.atykhan@nu.edu.kz>
2020-10-16 14:05:21 +0600

    trial

--- recursion/fib.py
+++ recursion/fib.py
@@ -1,2 +1,17 @@
 def fib(num):
     # return n-th Fibonacci number
+    fprev = 0
+    fprev2 = 1
+    if num == 0:
+    	return(0)
+    elif num == 1:
+    	return(1)
+    else:
+    	for i in range (num-1):
+    		fsled = fprev2 + fprev
+    		fprev = fprev2
+    		fprev2 = fsled
+    	return(fprev)
+
+print(fib(11))
+
