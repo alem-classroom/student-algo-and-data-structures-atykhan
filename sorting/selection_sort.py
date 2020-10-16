59225c47bde19c796f62ca3b63702d618b8098ab
atykhan <magzhan.atykhan@nu.edu.kz>
2020-10-16 12:45:59 +0600

    trial

--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -1,2 +1,19 @@
 def selection_sort(lst):
     # sort the list via selection sort
+	a = int(len(lst))
+	bmin = 0
+	newlst = []
+	print(lst)
+	if a == 1:
+		return(lst)
+	elif a > 1:
+		for i in range (a):
+			bmin = i
+			for j in range(i+1, len(lst)):
+				if lst[bmin] > lst[j]:
+					bmin = j
+			lst[i], lst[bmin] = lst[bmin], lst[i]
+		return(lst)
+
+listt = [5,2,3,1,3,9,7,2,6]
+print(selection_sort(listt))
