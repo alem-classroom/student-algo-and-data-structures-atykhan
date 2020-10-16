3c698f1f15582ef28f139c322fa04250bd6b04f5
atykhan <magzhan.atykhan@nu.edu.kz>
2020-10-16 14:51:45 +0600

    trial'

--- dynamic-programming-2/coins.py
+++ dynamic-programming-2/coins.py
@@ -1,2 +1,7 @@
 def maxCoins(lst, n):
     # return maximum number of coins that can be acquired in the maze
+	maxCoin = ((lst[0]+lst[1])-2)*n
+	return(maxCoin)
+# lit = [8,8]
+# n = 1
+# print(maxCoins(lit,n))
