bc7690333a35660cbb1af2ff1a495a65d64b1621
atykhan <magzhan.atykhan@nu.edu.kz>
2020-10-27 12:21:32 +0600

    'trial

--- node-linkedlist/node.py
+++ node-linkedlist/node.py
@@ -1,5 +1,5 @@
 class Node:
     def __init__(self, value):
         # initialize node such that its value equals to value, and next is None
-    	self.element = value
+    	self.value = value
     	self.next = None
