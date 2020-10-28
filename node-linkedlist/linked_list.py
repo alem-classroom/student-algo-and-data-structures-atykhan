f52b360a3220c25714351cb26c8828a18dfd82d8
atykhan <magzhan.atykhan@nu.edu.kz>
2020-10-28 10:48:30 +0600

    triall

--- node-linkedlist/linked_list.py
+++ node-linkedlist/linked_list.py
@@ -22,10 +22,9 @@ class LinkedList:
     def insert_back(self, node):
         # insert node to the back of the Linked List
         new_node = Node(node)
-        cur = self.head
-        while cur.next != None:
-            cur = cur.next
-        cur.next= new_node
+        while self.next != None:
+            cur = self.next
+        self.next= new_node
     def get_last(self):
         # return last node of the Linked List
         cur = self.head
