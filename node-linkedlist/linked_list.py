9e28ec8ae956840be18773946633c8f4efb21c0f
atykhan <magzhan.atykhan@nu.edu.kz>
2020-10-28 09:29:41 +0600

    trial

--- node-linkedlist/linked_list.py
+++ node-linkedlist/linked_list.py
@@ -7,16 +7,36 @@ class Node:
 class LinkedList:
     def __init__(self, head = None):
         # initialize Linked List by initializing head
+        self.head = head
     
     def get_head(self):
         # return head of the Linked List
-
+        cur = self.head
+        count = 0
+        while cur.next != None:
+            count +=count
+            if count == 1:
+                return(self.head)
+            break
+        return(self.head)
     def insert_back(self, node):
         # insert node to the back of the Linked List
-
+        new_node = node(node)
+        cur = self.head
+        while cur.next !=None:
+            cur = cur.next
+        cur.next= new_node
     def get_last(self):
         # return last node of the Linked List
-
+        cur = self.head
+        while cur.next == None:
+            return(self.head)
     def get_list(self):
         # create list and append every value of Linked List to it.
         # return the list
+        lst = []
+        cur = self.head
+        while cur.next != None:
+            lst.append(cur.next)
+        return(lst)
+            
