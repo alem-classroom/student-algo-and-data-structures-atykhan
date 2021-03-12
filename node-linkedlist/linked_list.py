63663faa73d612d3615d5664e707ef6eb9bc243f
atykhan <magzhan.atykhan@nu.edu.kz>
2021-03-12 14:17:07 +0600

    tria

--- node-linkedlist/linked_list.py
+++ node-linkedlist/linked_list.py
@@ -13,7 +13,7 @@ class LinkedList:
     def get_head(self):
         # return head of the Linked List
         cur_node = self.head
-        # cur_node = cur_node.next
+        cur_node = cur_node.next
         return(cur_node.value)
 
     def insert_back(self, node):
@@ -60,4 +60,4 @@ e2.next = e3
 print(my_list.get_list())
 print(len(my_list.get_list()))
 print(my_list.get_head())
-print(my_list.get_last())
+# print(my_list.get_last())
